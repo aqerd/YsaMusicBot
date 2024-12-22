51b69d33ba1dd8e59e6c30d322b908c9b934328d
Ruslan Suleymanov <87182605+aqerd@users.noreply.github.com>
2024-12-22 17:52:16 +0500

    Function for execution in TG

--- src/main/java/rs/aqerd/Bot.java
+++ src/main/java/rs/aqerd/Bot.java
@@ -5,6 +5,7 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 import static rs.aqerd.shared.Environment.getTelegramToken;
 import static rs.aqerd.shared.Environment.getTelegramUsername;
 
@@ -14,18 +15,24 @@ public class Bot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             Message message = update.getMessage();
             String text = message.getText();
+            String userId = String.valueOf(message.getChatId());
+
             System.out.println(text);
 
-            SendMessage sendMessage = SendMessage.builder()
-                    .chatId(String.valueOf(message.getChatId()))
-                    .text(text)
-                    .build();
+            executeMessage(text, userId);
+        }
+    }
 
-            try {
-                execute(sendMessage);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
+    public void executeMessage(String text, String userId) {
+        SendMessage sendMessage = SendMessage.builder()
+                .chatId(userId)
+                .text(text)
+                .build();
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
     }
 
